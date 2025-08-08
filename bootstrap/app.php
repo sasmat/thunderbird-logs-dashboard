b1c0ec3be4f478245ee5127a81a35f96865a5793
Shehroz Asmat <sasmat@trythunderbird.com>
2025-08-09 03:17:15 +0500

    Revert "Update app.php"
    This reverts commit d697df19e27a0c41d453e82a7e2666cdef21b8cd.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,16 +6,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        then: function () {
-            Route::prefix('thunderlog')
-                ->group(base_path('routes/web.php'));
-        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
